4e51b47c8415adcb78fe533396568fd709effa37
Diomar Villarroel <diomarfvr@gmail.com>
2025-02-05 18:59:18 -0400

    tailwindcss.config modified for new tailwindcss version fix 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
 import type { Config } from "tailwindcss"
-import { heroui } from "@heroui/react"
+// import { heroui } from "@heroui/react"
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    //"./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {},
     },
   },
-  darkMode: "class",
-  plugins: [heroui()],
+  // darkMode: "class",
+  // plugins: [heroui()],
 } satisfies Config;
